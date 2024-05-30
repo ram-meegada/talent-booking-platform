6b26e117fd72141e6df3647065ba6a8c9259737e
jaskaransingh1 <jaskaransingh@apptunix.com>
2024-05-30 10:52:09 +0530

    export csv file apis create

--- artist_app/services/uploadMediaService.py
+++ artist_app/services/uploadMediaService.py
@@ -2,6 +2,7 @@ from artist_app.models.uploadMediaModel import UploadMediaModel
 from artist_app.utils.saveImage import save_image
 from artist_app.serializers.uploadMediaSerializer import CreateUpdateUploadMediaSerializer
 from artist_app.utils import messages
+import mimetypes
 
 class UploadMediaService:
     def upload_media(self, request):
@@ -29,3 +30,25 @@ class UploadMediaService:
             return {"data": response_data, "message": messages.MEDIA_UPLOADED, "status": 200}
         except Exception as error:
             return {"data": "", "message": messages.WENT_WRONG, "status": 400}
+
+    def create_upload_media_xl(self, request, file):
+        media_list = []
+        
+        # Process the in-memory file object
+        image_url, image_name = save_image(file)
+        media = UploadMediaModel()
+        media.media_file_url = image_url
+        media.media_file_name = image_name
+        media.file_type = mimetypes.guess_type(file.name)[0]
+        if media.file_type is None:
+            media.file_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        media.save()
+
+        media_list.append(media)
+
+        serializer = CreateUpdateUploadMediaSerializer(media_list, many=True)
+        return {
+            "file_url": media.media_file_url,
+            "status": "success",
+            "message": "Excel file uploaded successfully."
+        }
