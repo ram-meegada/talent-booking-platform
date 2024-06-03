878ecec0de6f4b5fb525be83bba77d2352929920
jaskaransingh1 <jaskaransingh@apptunix.com>
2024-06-03 10:38:34 +0530

    jira issues

--- artist_project/urls.py
+++ artist_project/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path("admin/", include("artist_app.urls.adminUrls")),
 ]
 
-if DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+# if DEBUG:
+#     import debug_toolbar
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ]
