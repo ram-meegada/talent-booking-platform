17d7dd7beecc87078c198c9b4e86a6873ed94aaa
ram-meegada <ramumeegada1811@gmail.com>
2024-10-31 09:42:57 +0530

    added chat session

--- artist_app/models/chatSessionModel.py
+++ artist_app/models/chatSessionModel.py
@@ -5,6 +5,7 @@ from artist_app.models.userModel import UserModel
 
 class ChatSessionModel(BaseModel):
     session_id = models.CharField(max_length=100, blank=True, null=True)
+    testing = models.CharField(max_length=100, blank=True, null=True)
     client = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="client_user")
     talent = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="talent_user")
 
